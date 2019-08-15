93b7985a66be922f4a7e0d5354d6883e66308709
Luboš Mátl <matllubos@gmail.com>
2019-08-15 20:03:05 +0200

    Fixed Two factor authorization

--- auth_token/config.py
+++ auth_token/config.py
@@ -18,10 +18,12 @@ DEFAULTS = {
     'TWO_FACTOR_ENABLED': False,  # Two factor authentication is disabled
     'TWO_FACTOR_REDIRECT_URL': 'login-code-verification/',  # The path the user is redirected to after successful two
                                                             # factor authentication
-    'TWO_FACTOR_CODE_GENERATING_FUNCTION': 'auth_token.utils.generate_two_factor_code',  # Fuction, which generates code
-                                                                                         # for two factor authentication
+    'TWO_FACTOR_CODE_GENERATING_FUNCTION': 'auth_token.utils.generate_two_factor_code',  # Function, which generates
+                                                                                         # code for two factor
+                                                                                         # authentication
     'TWO_FACTOR_SENDING_FUNCTION': '',  # Function, which need to be implemented to send the key for second part of
                                         # authorization process to the user
+    'TWO_FACTOR_DEBUG_TOKEN_SMS_CODE': None,  # Default code for two factor debug
     'RENEWAL_EXEMPT_HEADER': 'X-Authorization-Renewal-Exempt',  # Header name which causes that the token expiration
                                                                 # time will not be extended
     'EXPIRATION_HEADER': 'X-Authorization-Expiration',  # Header name which contains information about token expiration
