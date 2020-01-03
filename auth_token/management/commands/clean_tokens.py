99ac6b1f346b1192f200ccfee0a26284cd251347
Lubos Matl <lubos.matl@mallpay.cz>
2020-01-03 18:29:20 +0100

    Faster clean tokens command

--- auth_token/management/commands/clean_tokens.py
+++ auth_token/management/commands/clean_tokens.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.db.models import Count
 from django.conf import settings as django_settings
 from django.utils import timezone
 from django.core.management.base import BaseCommand
@@ -12,10 +13,14 @@ from auth_token.models import Token
 class Command(BaseCommand):
 
     def handle(self, **options):
-        for user in apps.get_model(*django_settings.AUTH_USER_MODEL.split('.', 1)).objects.all():
+        cleaned_users = apps.get_model(*django_settings.AUTH_USER_MODEL.split('.', 1)).objects.annotate(
+            count_tokens=Count('auth_token')
+        ).filter(count_tokens__gt=settings.COUNT_USER_PRESERVED_TOKENS)
+
+        for user in cleaned_users:
             user_last_preserved_token_pks = Token.objects.filter(
                 user=user
-            ).order_by('-created_at')[:settings.COUNT_USER_PRESERVED_TOKENS].values_list('pk', flat=True)
+            ).order_by('-created_at')[:settings.COUNT_USER_PRESERVED_TOKENS].values('pk')
             removing_tokens_qs = Token.objects.filter(
                 last_access__lt=timezone.now() - timedelta(seconds=settings.MAX_TOKEN_AGE),
                 user=user
