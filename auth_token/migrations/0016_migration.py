5e1fe4db832bbb85706a9880889212142381a381
Luboš Mátl <matllubos@gmail.com>
2022-11-24 17:05:49 +0100

    Fixed missing verbose name in migration

--- auth_token/migrations/0016_migration.py
+++ auth_token/migrations/0016_migration.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='mobiledevice',
             name='uuid',
-            field=models.CharField(db_index=True, max_length=36),
+            field=models.CharField(db_index=True, max_length=36, verbose_name='UUID'),
         ),
     ]
